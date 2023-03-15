66841aca4a93803ce958d0d80a2e340c40b1d1db
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-03-15 15:12:53 -0500

    Time: 325 ms, Memory: 52.6 MB - LeetHub

---
+++ closest-binary-search-tree-value/closest-binary-search-tree-value.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function(root, target) {
+    let nodeVal = root.val;
+    let diff = Math.abs(root.val - target);
+    
+    let close = (node) => {
+        console.log("node", node);
+        console.log("nodeVal", nodeVal);
+        console.log("diff", diff);
+        let temp = Math.abs(node.val - target);
+           if(temp < diff) {
+               nodeVal = node.val;
+               diff = temp;
+           }
+        
+        if(node.left) {
+                close(node.left);
+        }
+        
+        if(node.right) {
+                close(node.right);
+        }
+    }
+    
+   close(root);
+    
+    return nodeVal;
+};
