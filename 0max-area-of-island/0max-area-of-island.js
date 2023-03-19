ad61641df04fa273f5a54186673b4d772ac64d50
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-03-18 21:08:16 -0500

    Time: 318 ms, Memory: 51.8 MB - LeetHub

---
+++ 0max-area-of-island/0max-area-of-island.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    let maxArea = 0;
+    let temp = 0;
+    
+    let valid = (row, col) => {
+      return 0 <= row && 0 <= col && row < rowLength && col < colLength && grid[row][col] == 1;
+    };
+    
+    let dfs = (row, col) => {
+        console.log("row & col", row, col);
+        
+        for(let [dx, dy] of directions) {
+            let nextRow = row + dy, nextCol = col + dx;
+            
+            if(valid(nextRow, nextCol) && !seen[nextRow][nextCol]) {
+                temp++;
+                seen[nextRow][nextCol] = true;
+                dfs(nextRow, nextCol);
+            }
+        }
+        
+        console.log("temp", temp);
+        
+        if(temp > maxArea) {
+            maxArea = temp;
+        }
+    };
+    
+    let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    let rowLength = grid.length;
+    let colLength = grid[0].length;
+    let seen = [];
+    
+    for(let i = 0; i < rowLength; i++) {
+        seen.push(new Array(colLength).fill(false));
+    }
+    
+    for(let row = 0; row < rowLength; row++) {
+        for(let col = 0; col < colLength; col++) {
+            if(grid[row][col] == 1 && !seen[row][col]) {
+                console.log("calling row & col", row, col);
+                temp = 1;
+                seen[row][col] = true;
+                dfs(row, col);
+            }
+        }
+    }
+    
+    return maxArea;
+};
